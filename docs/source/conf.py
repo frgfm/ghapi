f7964f6908d4bcc4293e732fd97630ff6e82957d
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-12-20 00:49:21 +0100

    docs: Fixed headers

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2022, François-Guillaume Fernandez.
 
-# This program is licensed under the Apache License version 2.
+# This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
 # Configuration file for the Sphinx documentation builder.
