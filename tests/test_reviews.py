fdbbf3754b2caba751ab36e757eb9d7c8c48004f
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-12-21 02:17:51 +0100

    test: Adds unittest

---
+++ tests/test_reviews.py
@@ -0,0 +1,44 @@
+import pytest
+
+from ghapi_client.exceptions import HTTPRequestException
+from ghapi_client.pulls import PullRequest
+from ghapi_client.reviews import Review
+
+
+@pytest.mark.parametrize(
+    "repo_name, pull_number, path, body, line, kwargs, expected_comment",
+    [
+        ["owner/repo", 2, "README.md", "Weird", 1, {}, {"path": "README.md", "body": "Weird", "line": 1}],
+        [
+            "owner/repo",
+            2,
+            "README.md",
+            "Weird",
+            3,
+            {"start_line": 1},
+            {"path": "README.md", "body": "Weird", "line": 3, "start_line": 1},
+        ],
+    ],
+)
+def test_review_stage_comment(repo_name, pull_number, path, body, line, kwargs, expected_comment):
+    pr = PullRequest(repo_name, pull_number)
+    review = Review(pr, "DUMMY_TOKEN")
+    assert len(review.pending_comments) == 0
+    review.stage_comment(path, body, line, **kwargs)
+    assert len(review.pending_comments) == 1
+    assert review.pending_comments[0] == expected_comment
+
+
+@pytest.mark.parametrize(
+    "repo_name, pull_number, path, body, line, kwargs, expected_comment",
+    [
+        ["owner/repo", 2, "README.md", "Weird", 1, {}, {"path": "README.md", "body": "Weird", "line": 1}],
+    ],
+)
+def test_review_submit(repo_name, pull_number, path, body, line, kwargs, expected_comment):
+    pr = PullRequest(repo_name, pull_number)
+    review = Review(pr, "DUMMY_TOKEN")
+    review.stage_comment(path, body, line, **kwargs)
+
+    with pytest.raises(HTTPRequestException):
+        review.submit("Thanks for the PR!")
