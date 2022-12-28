6849a85e06eb2f95e7c0ca31067cb71cfdfe6337
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-12-28 19:59:25 +0100

    test: Adds unittest

--- tests/test_repos.py
+++ tests/test_repos.py
@@ -38,3 +38,16 @@ def test_repository_list_pulls(owner, name, expected_error):
     else:
         with pytest.raises(expected_error):
             Repository(owner, name).list_pulls()
+
+
+@pytest.mark.parametrize(
+    "owner, repo, payload_len, created_at",
+    [
+        ["frgfm", "ghapi", 12, "2022-12-19T20:52:23Z"],
+    ],
+)
+def test_repo_get_info(owner, repo, payload_len, created_at):
+    repo = Repository(owner, repo)
+    out = repo.get_info()
+    assert len(out) == payload_len
+    assert out["created_at"] == created_at
