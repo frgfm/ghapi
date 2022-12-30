900a12648bcf162c79b5bea3cea2f37a372898ed
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-12-30 16:12:04 +0100

    test: Refactored user unittests

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,3 +16,10 @@ def mock_pull():
     with open(os.path.join("tests", "fixtures", "pull.json"), "rb") as f:
         response = json.load(f)
     return response
+
+
+@pytest.fixture(scope="session")
+def mock_user():
+    with open(os.path.join("tests", "fixtures", "user.json"), "rb") as f:
+        response = json.load(f)
+    return response
--- tests/test_users.py
+++ tests/test_users.py
@@ -22,31 +22,30 @@ def test_user_constructor(username, expected_error):
             User(username)
 
 
-@pytest.mark.parametrize(
-    "username, payload_len, created_at",
-    [
-        ["frgfm", 12, "2017-04-05T13:34:38Z"],
-    ],
-)
-def test_user_get_info(username, payload_len, created_at):
-    user = User(username)
+def test_user_get_info(mock_user):
+    conn = Connection(url="https://www.github.com")
+    user = User("frgfm", conn)
+    # Wrong api url
+    with pytest.raises(HTTPRequestException):
+        user.get_info()
+    # Fix url
+    user.conn.url = "https://api.github.com"
+    # Set response
+    user._info = mock_user
     out = user.get_info()
-    assert len(out) == payload_len
-    assert out["created_at"] == created_at
+    assert len(out) == 12
+    assert out["created_at"] == "2017-04-05T13:34:38Z"
 
 
-@pytest.mark.parametrize(
-    "username, expected_error",
-    [
-        ["imaginary_GH-Friend", HTTPRequestException],
-        ["frgfm", None],
-    ],
-)
-def test_user_list_repos(username, expected_error):
-    if expected_error is None:
-        repos = User(username).list_repos()
-        assert isinstance(repos, list) and len(repos) > 1
-        assert all(isinstance(elt, str) and len(elt) >= 1 for elt in repos)
-    else:
-        with pytest.raises(expected_error):
-            User(username).list_repos()
+def test_user_list_repos(mock_repo):
+    conn = Connection(url="https://www.github.com")
+    user = User("frgfm", conn)
+    # Wrong api url
+    with pytest.raises(HTTPRequestException):
+        user.list_repos()
+    # Fix url
+    user.conn.url = "https://api.github.com"
+    # Set response
+    user._repos = [mock_repo]
+    out = user.list_repos()
+    assert len(out) == 1 and out[0] == "frgfm/ghapi"
