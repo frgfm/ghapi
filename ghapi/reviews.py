23a5f565dbe28342cd8b6d2928ca8438ed7bb380
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-12-30 18:41:57 +0100

    refactor: Refactored Review

--- ghapi/reviews.py
+++ ghapi/reviews.py
@@ -4,15 +4,19 @@
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
 
 from enum import Enum
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Type, TypeVar, Union
 
 import requests
 
 from .connection import Connection
 from .exceptions import verify_status
 from .pulls import PullRequest
+from .utils import parse_comment, parse_review
 
-__all__ = ["Review"]
+__all__ = ["Comment", "Review"]
+
+
+T = TypeVar("T", bound="Review")
 
 
 class ReviewAction(str, Enum):
@@ -21,67 +25,162 @@ class ReviewAction(str, Enum):
     COMMENT = "COMMENT"
 
 
+class Comment:
+    """Implements a comment object
+
+    Args:
+        path: the relative path to the file that necessitates a comment.
+        body: the text of the review comment.
+        line: the line of the blob in the pull request diff that the comment applies to. For a multi-line comment,
+            the last line of the range that your comment applies to.
+        kwargs: keyword arguments for comment (cf. `Review creation
+            <https://docs.github.com/en/rest/pulls/reviews#create-a-review-for-a-pull-request--parameters>`_)
+    """
+
+    def __init__(self, path: str, body: str, line: int, **kwargs: Any) -> None:
+        self.path = path
+        self.body = body
+        self.line = line
+        self.kwargs = kwargs
+
+    def to_dict(self) -> Dict[str, Any]:
+        """Dumps the comment into a dictionary processable by GitHub API"""
+        return dict(path=self.path, body=self.body, line=self.line, **self.kwargs)
+
+
 class Review:
     r"""Implements a Review object
 
     >>> from ghapi import Repository, PullRequest, Review
-    >>> pr = PullRequest(Repository("frgfm", "torch-cam"), 187)
-    >>> pr.conn.set_token("MY_DUMMY_TOKEN")
-    >>> review = Review(pr)
-    >>> review.stage_comment("README.md", "This is weird!", 9)
-    >>> review.submit("Thanks for the PR!\nI left a few comments!")
+    >>> pr = PullRequest(Repository("frgfm", "Holocron"), 260)
+    >>> review = Review(pr, 1212580495)
+    >>> review.get_info()
 
     Args:
         pr: a pull request object
+        review_id: the review identifier
         conn: connection object
     """
 
     ROUTES: Dict[str, str] = {
         "create": "/repos/{owner}/{repo}/pulls/{pull_number}/reviews",
+        "info": "/repos/{owner}/{repo}/pulls/{pull_number}/reviews/{review_id}",
+        "comments": "/repos/{owner}/{repo}/pulls/{pull_number}/reviews/{review_id}/comments",
     }
 
-    def __init__(self, pr: PullRequest, conn: Union[Connection, None] = None) -> None:
+    def __init__(self, pr: PullRequest, review_id: int, conn: Union[Connection, None] = None) -> None:
         self.pr = pr
+        self.review_id = review_id
         self.conn = conn if isinstance(conn, Connection) else pr.conn
-        # Check that the connection is valid (accessing the token property will check that itself)
-        isinstance(self.conn.token, str)
-        # Create the pending review
-        self.pending_comments: List[Dict[str, Any]] = []
+        self.reset()
 
-    def stage_comment(self, path: str, body: str, line: int, **kwargs: Any) -> None:
-        """Stage a comment for the review
+    def reset(self) -> None:
+        self._info: Union[Dict[str, Any], None] = None
+        self._comments: Union[List[Dict[str, Any]], None] = None
+
+    @property
+    def info(self) -> Dict[str, Any]:
+        if not isinstance(self._info, dict):
+            self._info = verify_status(
+                requests.get(
+                    self.conn.resolve(
+                        self.ROUTES["info"].format(
+                            owner=self.pr.repo.owner,
+                            repo=self.pr.repo.name,
+                            pull_number=self.pr.pull_number,
+                            review_id=self.review_id,
+                        )
+                    )
+                ),
+                200,
+            ).json()
+        return self._info
+
+    def get_info(self) -> Dict[str, Any]:
+        """Parses high-level information from the review payload"""
+        return parse_review(self.info)
+
+    def _list_comments(self, **kwargs: Any) -> List[Dict[str, Any]]:
+        if not isinstance(self._comments, list):
+            self._comments = verify_status(
+                requests.get(
+                    self.conn.resolve(
+                        self.ROUTES["comments"].format(
+                            owner=self.pr.repo.owner,
+                            repo=self.pr.repo.name,
+                            pull_number=self.pr.pull_number,
+                            review_id=self.review_id,
+                        )
+                    ),
+                    params=kwargs,
+                ),
+                200,
+            ).json()
+        return self._comments
+
+    def list_comments(self, **kwargs: Any) -> List[Dict[str, Any]]:
+        """List the comments of a Pull Request Review.
 
         Args:
-            path: the relative path to the file that necessitates a comment.
-            body: the text of the review comment.
-            line: the line of the blob in the pull request diff that the comment applies to. For a multi-line comment,
-                the last line of the range that your comment applies to.
-            kwargs: keyword arguments for comment creation
+            kwargs: query parameters of `List review comments
+                <https://docs.github.com/en/rest/pulls/reviews#list-comments-for-a-pull-request-review>`_
+        Returns:
+            list of comments
         """
-        self.pending_comments.append(dict(path=path, body=body, line=line, **kwargs))
+        return [parse_comment(comment) for comment in self._list_comments(**kwargs)]
 
-    def submit(self, body: str, action: ReviewAction = ReviewAction.COMMENT) -> None:
-        """Submit a review
+    @classmethod
+    def from_comments(
+        cls: Type[T],
+        pr: PullRequest,
+        body: str,
+        comments: List[Comment],
+        action: ReviewAction = ReviewAction.COMMENT,
+        conn: Union[Connection, None] = None,
+    ) -> T:
+        """Submit a review from comments
+
+        >>> from ghapi import Repository, PullRequest, Review, Comment
+        >>> pr = PullRequest(Repository("owner-login", "awesome-repo"), 1)
+        >>> pr.conn.set_token("MY_DUMMY_TOKEN")
+        >>> comment = Comment("README.md", "This is weird!", 9)
+        >>> review = Review.from_comments(pr, "Thanks for the PR :pray:", [comment])
 
         Args:
+            pr: a pull request object
             body: the body text of the pull request review.
+            comments: the review comments
             action: the review action you want to perform.
+            conn: connection object
         """
-        self.response = verify_status(
+
+        # Create the object
+        review = cls(pr, 1, conn)
+        # Check that the connection is valid (accessing the token property will check that itself)
+        isinstance(review.conn.token, str)
+
+        # Submit the PR
+        response = verify_status(
             requests.post(
-                self.conn.resolve(
-                    self.ROUTES["create"].format(
-                        owner=self.pr.repo.owner,
-                        repo=self.pr.repo.name,
-                        pull_number=self.pr.pull_number,
+                review.conn.resolve(
+                    cls.ROUTES["create"].format(
+                        owner=pr.repo.owner,
+                        repo=pr.repo.name,
+                        pull_number=pr.pull_number,
                     )
                 ),
                 json={
                     "body": body,
                     "event": action,
-                    "comments": self.pending_comments,
+                    "comments": [comment.to_dict() for comment in comments],
                 },
-                headers=self.conn.authorization,
+                headers=review.conn.authorization,
             ),
             200,
         ).json()
+        # Updated the review id
+        review.review_id = response["id"]
+        # Fill the cache
+        review._info = response
+
+        return review
