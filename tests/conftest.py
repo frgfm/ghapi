50b170b49144826291e50d734ef9331e148d84a0
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-12-30 16:48:49 +0100

    test: Refactored pull unittests

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,6 +19,27 @@ def mock_pull():
 
 
 @pytest.fixture(scope="session")
+def mock_pull_diff():
+    with open(os.path.join("tests", "fixtures", "pull_diff.json"), "rb") as f:
+        response = json.load(f)
+    return response
+
+
+@pytest.fixture(scope="session")
+def mock_review():
+    with open(os.path.join("tests", "fixtures", "review.json"), "rb") as f:
+        response = json.load(f)
+    return response
+
+
+@pytest.fixture(scope="session")
+def mock_comment():
+    with open(os.path.join("tests", "fixtures", "comment.json"), "rb") as f:
+        response = json.load(f)
+    return response
+
+
+@pytest.fixture(scope="session")
 def mock_user():
     with open(os.path.join("tests", "fixtures", "user.json"), "rb") as f:
         response = json.load(f)
